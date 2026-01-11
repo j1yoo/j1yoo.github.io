a455c25a7c9dabc99ba2cbdcd4feb349cb1c72e7
j1yoo <jaewon.yoo@iss.nthu.edu.tw>
2026-01-11 18:28:22 +0000

    Deploying to gh-pages from @ j1yoo/j1yoo.github.io@b8185b3d56db23068057a8855d6649a5c5193ecb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -95,6 +95,17 @@ ninja.data = [{
             window.location.href = "/blog/2025/Three_Conceptual_Shifts_That_Will_Change_How_You_See_Causal_Inference/";
           
         },
+      },{id: "post-significance-stars-a-cautionary-tale",
+        
+          title: "Significance Stars: A Cautionary Tale",
+        
+        description: "P-values, power, and the three types of significance: a guide to interpreting quantitative evidence.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/Significance_Stars_A_Cautionary_Tale/";
+          
+        },
       },{id: "post-ddd-estimators-and-distributional-effects",
         
           title: 'DDD Estimators and Distributional Effects <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
